a63f1f93ea04f071f5658b95424abe8a18eb13f2
katfogy <katfogy@gmail.com>
2023-07-27 21:14:53 +0100

    Reviewers Changes effected

--- student.rb
+++ student.rb
@@ -2,18 +2,16 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, _classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = nil
-    self.classroom = classroom if classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯\(ツ)/¯'
-  end
-
-  def enroll_in_classroom(classroom)
-    @classroom = classroom
-    classroom.add_student(self)
   end
 end

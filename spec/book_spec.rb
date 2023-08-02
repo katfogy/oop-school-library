802ccf4eb3e5b5c450cf39058b00df617aeca8d5
katfogy <katfogy@gmail.com>
2023-08-02 13:52:30 +0100

    book test file updatd

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../book'
 
 describe Book do
-  let(:book) { Book.new('The Lord of the Rings', 'J. R. R. Tolkien') }
+  let(:book) { Book.new('Introduction to CyberSecurity', 'NSUK') }
 
   describe '#initialize' do
     it 'creates a new book' do
@@ -19,8 +19,8 @@ describe Book do
     it 'serializes a book' do
       expect(book.serialize).to eq(
         {
-          'title' => 'The Lord of the Rings',
-          'author' => 'J. R. R. Tolkien',
+          'title' => 'Introduction to CyberSecurity',
+          'author' => 'NSUK',
           'id' => book.id
         }
       )
